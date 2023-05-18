e11a974c4803a0c206be69b3a88e0600f8e707f3
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-18 06:40:31 +0200

    Tests pass auto commit.

--- animate.py
+++ animate.py
@@ -3,13 +3,24 @@ class Renderer:
     """
     >>> renderer = Renderer()
     >>> renderer.render(animation=TestAnimation(), destination="animation.mp4")
+    Write frame1.png
+    Write frame2.png
+    Write frame3.png
+    Compose frame1.png frame2.png frame3.png
     """
 
     def render(self, animation, destination):
-        pass
+        frames = []
+        for index in range(animation.get_number_of_frames()):
+            frame = f"frame{index+1}.png"
+            frames.append(frame)
+            print(f"Write {frame}")
+        print(f"Compose {' '.join(frames)}")
 
 class TestAnimation:
-    pass
+
+    def get_number_of_frames(self):
+        return 3
 
 if __name__ == "__main__":
     print("Animate")
