eab9dcd5701656d14fd002d9913862d8751c7867
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-18 06:34:40 +0200

    Tests pass auto commit.

--- animate.py
+++ animate.py
@@ -1,7 +1,12 @@
 class Animation:
 
     """
+    >>> animation = Animation()
+    >>> animation.render("animation.mp4")
     """
+
+    def render(self, path):
+        pass
 
 if __name__ == "__main__":
     print("Animate")
