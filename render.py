8dc4c9307987d3ea66e97af2a35b2fc22dfc30a2
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 07:09:30 +0200

    Tests pass auto commit.

--- render.py
+++ render.py
@@ -1,3 +1,4 @@
+from animations.test import TestAnimation
 from graphics import Graphics
 from process import Process
 
@@ -57,19 +58,3 @@ class VideoRenderer:
             "-pix_fmt", "yuv420p",
             destination,
         ])
-
-class TestAnimation:
-
-    def get_duration_in_ms(self):
-        return 3000
-
-    def reset(self):
-        print("Reset")
-        self.x = 10
-
-    def update(self, elapsed_ms):
-        print(f"Update {elapsed_ms}")
-        self.x += elapsed_ms*0.1
-
-    def draw(self, surface):
-        surface.fill_rect(self.x, 10, 10, 10, (0.1, 0.5, 0.8))
