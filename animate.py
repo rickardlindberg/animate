67e9a44f98700f7f18f0898e4695f17817a3f7b6
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 07:31:04 +0200

    Tests pass auto commit.

--- animate.py
+++ animate.py
@@ -2,7 +2,7 @@ from animations.test import TestAnimation
 from preview import Preview
 
 if __name__ == "__main__":
-    Preview.create().render(
+    Preview.create().run(
         animation=TestAnimation(),
         destination="/tmp/animation.mp4",
         fps=25,
