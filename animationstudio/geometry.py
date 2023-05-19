af941e3c9e3553e0f816d1cb1df167ebbc6c02ee
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 10:58:34 +0200

    Tests pass auto commit.

--- animationstudio/geometry.py
+++ animationstudio/geometry.py
@@ -12,5 +12,14 @@ class Size(namedtuple("Size", "width,height")):
             y=(self.height-size.height)//2,
         )
 
+    def scale_factor(self, size):
+        """
+        >>> Size(200, 200).scale_factor(Size(400, 400))
+        2.0
+        """
+        width_scale_factor = size.width / self.width
+        height_scale_factor = size.height / self.height
+        return min(width_scale_factor, height_scale_factor)
+
 class Point(namedtuple("Pint", "x,y")):
     pass
