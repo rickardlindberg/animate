1ecc52b4015d2cf4d232be96846efa0691a4c295
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 07:08:23 +0200

    Tests pass auto commit.

--- preview.py
+++ preview.py
@@ -1,4 +1,18 @@
 class Preview:
 
+    """
+    """
+
+    @staticmethod
+    def create():
+        return Preview(pygame=pygame)
+
+    @staticmethod
+    def create_null():
+        return Preview(pygame=NullPygame())
+
+    def __init__(self, pygame):
+        self.pygame = pygame
+
     def run(self, animation):
         pass
