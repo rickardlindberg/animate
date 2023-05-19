dc7d2de0c511b9926cb05b4009a664411995bacf
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 09:41:20 +0200

    Tests pass auto commit.

--- animationstudio/preview.py
+++ animationstudio/preview.py
@@ -2,31 +2,9 @@ from animationstudio.graphics import Graphics
 
 import pygame
 
-class TestAnimation:
-
-    def get_duration_in_ms(self):
-        return 3000
-
-    def reset(self):
-        print("Reset")
-        self.x = 10
-
-    def update(self, elapsed_ms):
-        print(f"Update {elapsed_ms}")
-        self.x += elapsed_ms*0.1
-
-    def draw(self, surface):
-        surface.fill_rect(self.x, 10, 10, 10, (0.1, 0.5, 0.8))
-
 class Preview:
 
     """
-    >>> Preview.create_null().run(TestAnimation())
-    Reset
-    Update 0
-    FillRect =>
-      x: 10.0
-
     >>> isinstance(Preview.create(), Preview)
     True
     """
