6c4d18bcb8d0110a520498838c1d5a004be81e4a
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 10:55:26 +0200

    Tests pass auto commit.

--- animationstudio/geometry.py
+++ animationstudio/geometry.py
@@ -1,7 +1,16 @@
 from collections import namedtuple
 
 class Size(namedtuple("Size", "width,height")):
-    pass
+
+    def center(self, size):
+        """
+        >>> Size(400, 400).center(Size(200, 200))
+        Point(x=100, y=100)
+        """
+        return Point(
+            x=(self.width-size.width)//2,
+            y=(self.height-size.height)//2,
+        )
 
 class Point(namedtuple("Pint", "x,y")):
     pass
