2a11fb7edaffd96f23565b9e84e889deedaacbc1
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 15:34:40 +0200

    Tests pass auto commit.

--- animationstudio/__init__.py
+++ animationstudio/__init__.py
@@ -1,3 +1,6 @@
+"""
+"""
+
 from animationstudio.animation import Animation
 from animationstudio.geometry import Size
 from animationstudio.preview import Preview
@@ -11,9 +14,9 @@ __all__ = [
     "SIZE_720",
 ]
 
-SIZE_4K = Size(width=300, height=300)
+SIZE_4K = Size(width=3840, height=2160)
 SIZE_HD = SIZE_4K.scale(0.5)
-SIZE_720 = SIZE_HD.scale(0.5)
+SIZE_720 = SIZE_HD.scale(0.666667)
 
 def run(animation):
     animation.dry_run()
