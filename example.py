505c35aa0ff0b4c54fff6085539b38c0741c29f4
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 11:14:01 +0200

    Tests pass auto commit.

--- example.py
+++ example.py
@@ -18,7 +18,6 @@ class ExampleAnimation(Animation):
     """
 
     def get_size(self):
-        return Size(width=300, height=300)
         return Size(width=1920, height=1080)
 
     def get_duration_in_ms(self):
