e5e96f95ebd2a671e66ef21b9bbfec4bac50cd21
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 10:43:43 +0200

    Tests pass auto commit.

--- animationstudio/__init__.py
+++ animationstudio/__init__.py
@@ -1,7 +1,12 @@
 from animationstudio.animation import Animation
+from animationstudio.geometry import Size
 from animationstudio.preview import Preview
 
-__all__ = ["run", "Animation"]
+__all__ = [
+    "run",
+    "Animation",
+    "Size",
+]
 
 def run(animation):
     animation.dry_run()
