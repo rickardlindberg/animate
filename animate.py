2b70c71f1204bfc56cfea25a3f392fed944f565f
Rickard Lindberg <rickard@rickardlindberg.me>
2023-05-19 08:45:54 +0200

    Tests pass auto commit.

--- animate.py
+++ animate.py
@@ -1,5 +1,18 @@
-from animations.test import TestAnimation
 import animationstudio
+
+class TestAnimation:
+
+    def get_duration_in_ms(self):
+        return 3000
+
+    def reset(self):
+        self.x = 10
+
+    def update(self, elapsed_ms):
+        self.x += elapsed_ms*0.1
+
+    def draw(self, surface):
+        surface.fill_rect(self.x, 10, 10, 10, (0.1, 0.5, 0.8))
 
 if __name__ == "__main__":
     animationstudio.run(TestAnimation())
